7d74fea52f72962709efbb22573a57c58f99a59c
“Bethany <Bethanygellington@gmail.com>
2023-01-10 10:06:50 +0800

    add bonus mission no 3

--- src/org/launchcode/java/studios/areaofacircle/CharacterCounter.java
+++ src/org/launchcode/java/studios/areaofacircle/CharacterCounter.java
@@ -3,12 +3,30 @@ package org.launchcode.java.studios.areaofacircle;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+
+
 public class CharacterCounter  {
     public static void main(String[] args) {
 // have an input or test string
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a string: ");
         String input = scanner.nextLine().toLowerCase();
+//        String inputStripped = input.matches("[^a-zA-Z_0-9]");
+        String inputStripped = input.replaceAll("[^a-z]", "");
+
+
+//    HashMap<Character, Integer> characterCount = new HashMap<>();
+//        String testString = "Pokemon, Gotta catch em all";
+//        char[] charactersInString = testString.toCharArray();
+//
+//        for (char character : charactersInString) {
+//            if (characterCount.containsKey(character)){
+//                characterCount.put(character, characterCount.get(character) + 1);
+//            }
+//            else {
+//                characterCount.put(character, 1);
+//            }
+//        }
 
 //        String testString = "If the product of two terms is zero then " +
 //                "common sense says at least one of the two terms has to be zero to start with. " +
@@ -17,21 +35,20 @@ public class CharacterCounter  {
 //                "pretty straightforward from there.";
 
 // create a Map to store the character counts. Below
-        Map<Character, Integer> charCounts = new HashMap<>();
-        // split string into char array and iterate through with a for loop
-        for (char i : input.toCharArray()) {
-            // If the character is not in the hashMap we made above, add it with a count of 1
+        var charCounts = new HashMap<Character, Integer>();
+//         split string into char array and iterate through with a for loop
+        for (char i : inputStripped.toCharArray()) {
+//             If the character is not in the hashMap we made above, add it with a count of 1
             if (!charCounts.containsKey(i)) {
                 charCounts.put(i, 1);
             }
-            // If the character is already in the map, increment its count
+//             If the character is already in the map, increment its count
             else {
                 charCounts.put(i, charCounts.get(i) + 1);
             }
         }
 
         // Print the character counts
-        //below also works if change Map to HashMap, why?
         for (Map.Entry<Character, Integer> entry : charCounts.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }
