e307409f3115fc22f14b61270966814a5d235fa4
“Bethany <Bethanygellington@gmail.com>
2023-01-12 13:30:39 +0800

    code chap 4 school exercises

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -1,7 +1,15 @@
 package org.launchcode.java.demos.lsn3classes1;
 
+
 public class SchoolPractice {
     public static void main(String[] args) {
         // Instantiate your Student class for part 2 here!
+        Student pigeon = new Student();
+            pigeon.setNumberOfCredits(1);
+            pigeon.setGpa(4.0);
+            System.out.println(pigeon.getNumberOfCredits());
+            System.out.println(pigeon.getGpa());
+
+
     }
 }
---
+++ src/org/launchcode/java/demos/lsn3classes1/Teacher.java
@@ -0,0 +1,37 @@
+package org.launchcode.java.demos.lsn3classes1;
+
+public class Teacher {
+    private String firstName;
+    private String lastname;
+    private String subject;
+    private float yearsTeaching;
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastname() {
+        return lastname;
+    }
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+    public String getSubject() {
+        return subject;
+    }
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+    public float getYearsTeaching() {
+        return yearsTeaching;
+    }
+    public void setYearsTeaching(float yearsTeaching) {
+        this.yearsTeaching = yearsTeaching;
+    }
+
+
+
+
+}
